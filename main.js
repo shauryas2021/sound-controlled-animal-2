a0119dbe8bb4488a01688d905dded2d2965205fb
shauryas2021 <85066104+shauryas2021@users.noreply.github.com>
2022-03-14 21:04:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/OCCfj49wv/model.json', modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
